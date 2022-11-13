66b4ad17ae9ca4c8372a8407d4c96b0f8da5c226
Cristina Fores <cristinaforescampos1992@gmail.com>
2022-11-14 00:21:53 +0100

    Fix delete var no used form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable jsx-a11y/anchor-has-content */
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import useUser, { UserCredencials } from "../../hooks/useUser";
 import { FormStyled, FormField, ButtonStyled, FormActions } from "./FormStyled";
 
