e02e81a33eebf3a9160e2c8c8b5f51dd4bba925c
Cristina Fores <cristinaforescampos1992@gmail.com>
2022-11-14 00:04:57 +0100

    Create component Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import { AppStyled } from "./AppStyled";
+import Form from "./components/Form/Form";
 import Header from "./components/Header/Header";
-import useUser from "./hooks/useUser";
 
 function App() {
-  const { login } = useUser();
-  const user = {
-    username: "cristina",
-    password: "1234567891",
-  };
-
-  login(user);
   return (
     <>
       <AppStyled>
         <Header />
+        <Form />
       </AppStyled>
     </>
   );
