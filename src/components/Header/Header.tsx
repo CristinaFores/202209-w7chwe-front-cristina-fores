f86be590d1917c375d297f2597702d9894f7b2eb
Cristina Fores <cristinaforescampos1992@gmail.com>
2022-11-13 01:07:37 +0100

    Add compoent Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeaderStyled, HeaderTextStyled } from "./HeaderStyled";
+
+const Header = () => {
+  return (
+    <HeaderStyled>
+      <HeaderTextStyled>InstaFake</HeaderTextStyled>
+    </HeaderStyled>
+  );
+};
+
+export default Header;
