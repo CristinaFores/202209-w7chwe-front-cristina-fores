de5617f0be917ce244525f92201eb30c67566a25
Cristina Fores <cristinaforescampos1992@gmail.com>
2022-11-15 19:23:43 +0100

    Add Routers in app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,18 @@
 import { AppStyled } from "./AppStyled";
 import Form from "../Form/Form";
 import Header from "../Header/Header";
+import { Route, Routes } from "react-router-dom";
+import RegisterForm from "../RegisterForm/RegisterForm";
 
 function App() {
   return (
     <>
       <AppStyled>
         <Header />
-        <Form />
+        <Routes>
+          <Route path="/" element={<Form />} />
+          <Route path="/register" element={<RegisterForm />} />
+        </Routes>
       </AppStyled>
     </>
   );
