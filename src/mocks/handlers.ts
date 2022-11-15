7a1c21451ee7d6a120bfdec8914d433243bdd7d3
Cristina Fores <cristinaforescampos1992@gmail.com>
2022-11-15 18:14:24 +0100

    Add handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -6,4 +6,7 @@ export const handlers = [
   rest.post(`${url}/users/login`, async (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ token: "kitten" }));
   }),
+  rest.post(`${url}/users/register`, async (req, res, ctx) => {
+    return res(ctx.status(201), ctx.json({}));
+  }),
 ];
