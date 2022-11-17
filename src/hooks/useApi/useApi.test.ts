04746b82cd5878e015b43a94d156a02dd4d3b0df
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-17 23:43:04 +0100

    Fix test useApi

--- src/hooks/useApi/useApi.test.ts
+++ src/hooks/useApi/useApi.test.ts
@@ -1,5 +1,6 @@
 import { renderHook } from "@testing-library/react";
 import ProviderWrapper from "../../mocks/ProviderWrapper";
+import { loadUserListActionCreator } from "../../redux/features/loadUsers/userLoad";
 
 import { useAppDispatch } from "../../redux/hooks";
 
@@ -23,10 +24,34 @@ describe("Given the useApi custom hook", () => {
         wrapper: ProviderWrapper,
       });
 
+      const list = [
+        { id: "ohlya4yvyt", token: "8onmp9qs7z", username: "Ahmad.Wuckert57" },
+        { id: "n8g1hsospy", token: "pzy5c8v02l", username: "Khalil88" },
+        { id: "ck1y2an4zg", token: "1algbx44qe", username: "Brooke_Pagac" },
+        { id: "xux9uslw9c", token: "b5dhl8o34w", username: "Dandre.Tromp90" },
+        { id: "9jjd1l2bfn", token: "70h8yh8p8p", username: "Erling_Nader" },
+      ];
+
+      const expectList = {
+        payload: [
+          {
+            id: "ohlya4yvyt",
+            token: "8onmp9qs7z",
+            username: "Ahmad.Wuckert57",
+          },
+          { id: "n8g1hsospy", token: "pzy5c8v02l", username: "Khalil88" },
+          { id: "ck1y2an4zg", token: "1algbx44qe", username: "Brooke_Pagac" },
+          { id: "xux9uslw9c", token: "b5dhl8o34w", username: "Dandre.Tromp90" },
+          { id: "9jjd1l2bfn", token: "70h8yh8p8p", username: "Erling_Nader" },
+        ],
+        type: "userList/loadUserList",
+      };
+
       const dispatch = useAppDispatch();
       await loadUsers();
 
       expect(dispatch).toHaveBeenCalledTimes(1);
+      expect(expectList).toEqual(loadUserListActionCreator(list));
     });
   });
 });
